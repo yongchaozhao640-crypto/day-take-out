6373af9d741276fa219eb7935170af1f49418911
zyc <yongchaozhao640@gmail.com>
2026-03-14 10:24:04 +0800

    新增员工相关代码

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.sky.handler;
 
+import com.sky.constant.MessageConstant;
 import com.sky.exception.BaseException;
 import com.sky.result.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
 
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
@@ -23,5 +26,22 @@ public class GlobalExceptionHandler {
         log.error("异常信息：{}", ex.getMessage());
         return Result.error(ex.getMessage());
     }
-
+    /**
+     * 捕获用户登录重复
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler
+    public Result exceptionHandler(SQLIntegrityConstraintViolationException ex){
+        String message = ex.getMessage();
+        log.error("异常信息：{}", message);
+        String[] split = message.split(" ");
+        if(message.contains("Duplicate entry")){
+            String username=split[2];
+            String msg= username + MessageConstant.ALREADY_EXISTS;
+            return Result.error(msg);}
+        else{
+            return Result.error(MessageConstant.UNKNOWN_ERROR);
+        }
+    }
 }
